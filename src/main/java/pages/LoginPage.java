72d852d4e734dc19169505a4290668caeadb23cb
himaliweer <himaliweerakoon@gmail.com>
2026-03-15 14:47:46 +0530

    Log4j Commit

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -3,6 +3,8 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import utils.Log;
+
 public class LoginPage {
 	private WebDriver driver;
 
@@ -28,8 +30,11 @@ public class LoginPage {
 	}
 
 	public void clickLogin() {
+		Log.info("Clicking login button..");
 		driver.findElement(loginButton).click();
 	}
 }
+
+
 
 
