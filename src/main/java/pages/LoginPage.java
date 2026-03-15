931a739cef39dfddac94f141ab6d8187d12f4896
himaliweer <himaliweerakoon@gmail.com>
2026-03-15 12:51:16 +0530

    First Commit

---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,35 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+	private WebDriver driver;
+
+	private By emailTextbox = By.id("Email");
+	private By passwordTextbox = By.id("Password");
+
+	private By loginButton = By.xpath("//*[@id=\"main\"]/div/section/div/div[2]/div[1]/div/form/div[3]/button");
+
+	public LoginPage(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	public void enterEmail(String email) {
+		driver.findElement(emailTextbox).clear();
+		driver.findElement(emailTextbox).sendKeys(email);
+
+	}
+
+	public void enterPassword(String password) {
+		driver.findElement(passwordTextbox).clear();
+		driver.findElement(passwordTextbox).sendKeys(password);
+
+	}
+
+	public void clickLogin() {
+		driver.findElement(loginButton).click();
+	}
+}
+
+
