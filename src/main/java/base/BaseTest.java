c63ebb8b259216a7aa1bde098cb11079e68b6a3a
himaliweer <himaliweerakoon@gmail.com>
2026-03-16 19:05:53 +0530

    HTML reports with screenshots

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -3,12 +3,30 @@ package base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+
+import utils.ExtentReportManager;
 import utils.Log;
 
 public class BaseTest {
 	protected WebDriver driver;
+	protected static ExtentReports extent;
+	protected ExtentTest test;
+
+	@BeforeSuite
+	public void setupReport() {
+		extent = ExtentReportManager.getReportInstance();
+	}
+
+	@AfterSuite
+	public void tearDownReport() {
+		extent.flush();
+	}
 
 	@BeforeMethod
 	public void setUp() {
@@ -29,7 +47,3 @@ public class BaseTest {
 	}
 
 }
-
-
-
-
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 
 import base.BaseTest;
 import pages.LoginPage;
+import utils.ExtentReportManager;
 import utils.Log;
 
 public class LoginTest extends BaseTest{
@@ -13,20 +14,31 @@ public class LoginTest extends BaseTest{
 	public void testValidLogin() {
 		
 		Log.info("Starting login test..."); 
-		
+		test = ExtentReportManager.createTest("Login Test - ");
+
 		LoginPage loginPage = new LoginPage(driver);
-		
+		test.info("Navigating to URL");
 		Log.info("Adding credentials");
+		
+		
 		loginPage.enterEmail("admin@yourstore.com");
 		loginPage.enterPassword("admin");
 		
 		loginPage.clickLogin();
 		
+		test.info("Clicking on Login button");
+		
 		System.out.println("Title of the page is: " +driver.getTitle());
 		Log.info("Verifying page title");
+		test.info("Verifying page title");
 		Assert.assertEquals(driver.getTitle(), "Just a moment...");
+		
+		test.pass("Login Successful");
 	}
 	
 }
+
+
+
 
 
