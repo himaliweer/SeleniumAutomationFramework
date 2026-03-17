38510d0fd01d6c20cb3fa33e0a470672eace134a
himaliweer <himaliweerakoon@gmail.com>
2026-03-17 19:53:48 +0530

    Page Factory

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -2,39 +2,58 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import utils.Log;
 
 public class LoginPage {
 	private WebDriver driver;
 
+	// Page Factory
+	@FindBy(id="Email")
+	WebElement emailTextbox;
+	
+	@FindBy(id="Password")
+	WebElement passwordTextbox;
+	
+	@FindBy(xpath="//*[@id=\"main\"]/div/section/div/div[2]/div[1]/div/form/div[3]/button")
+	WebElement loginButton;
+	
+	/*
 	private By emailTextbox = By.id("Email");
 	private By passwordTextbox = By.id("Password");
 
 	private By loginButton = By.xpath("//*[@id=\"main\"]/div/section/div/div[2]/div[1]/div/form/div[3]/button");
-
+	*/
+	
 	public LoginPage(WebDriver driver) {
 		this.driver = driver;
+		PageFactory.initElements(driver, this);
 	}
 
 	public void enterEmail(String email) {
+		emailTextbox.clear();
+		emailTextbox.sendKeys(email);
+		/*
 		driver.findElement(emailTextbox).clear();
 		driver.findElement(emailTextbox).sendKeys(email);
-
+		*/
 	}
 
 	public void enterPassword(String password) {
+		passwordTextbox.clear();
+		passwordTextbox.sendKeys(password);
+		/*
 		driver.findElement(passwordTextbox).clear();
 		driver.findElement(passwordTextbox).sendKeys(password);
-
+		*/	
 	}
 
 	public void clickLogin() {
 		Log.info("Clicking login button..");
-		driver.findElement(loginButton).click();
+		loginButton.click();
+		//driver.findElement(loginButton).click();
 	}
 }
-
-
-
-
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -31,7 +31,7 @@ public class LoginTest extends BaseTest{
 		System.out.println("Title of the page is: " +driver.getTitle());
 		Log.info("Verifying page title");
 		test.info("Verifying page title");
-		Assert.assertEquals(driver.getTitle(), "Just a moment... 123");
+		Assert.assertEquals(driver.getTitle(), "Just a moment...");
 		
 		test.pass("Login Successful");
 	}
